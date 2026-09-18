a3db6bb2172c67573fe4c6ffadfba8a64e7bda6a
algive1 <97030462@qq.com>
2026-09-19 01:19:46 +0800

    fix: import dataset binding extension for auto backup

--- lib/features/account/application/personal_cloud_auto_backup_service.dart
+++ lib/features/account/application/personal_cloud_auto_backup_service.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../core/database/app_database.dart';
 import '../../../core/database/database_provider.dart';
 import '../../sharing/data/session_repository.dart';
 import 'personal_cloud_backup_service.dart';
