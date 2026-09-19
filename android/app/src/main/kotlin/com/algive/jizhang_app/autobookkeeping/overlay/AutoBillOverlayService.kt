a336e5bea8c6619ace5a696910d651c59593e396
algive1 <97030462@qq.com>
2026-09-20 01:50:02 +0800

    chore: remove obsolete overlay SDK branch import

--- android/app/src/main/kotlin/com/algive/jizhang_app/autobookkeeping/overlay/AutoBillOverlayService.kt
+++ android/app/src/main/kotlin/com/algive/jizhang_app/autobookkeeping/overlay/AutoBillOverlayService.kt
@@ -4,7 +4,6 @@ import android.app.Service
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.PixelFormat
-import android.os.Build
 import android.os.IBinder
 import android.util.Log
 import android.view.Gravity
