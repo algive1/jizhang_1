0ba9117a0215198190a1f3ef7e2291a77880a0dd
algive1 <97030462@qq.com>
2026-09-20 19:20:08 +0800

    fix: repair imported credit key normalization syntax

--- lib/features/insights/domain/financial_insight_engine.dart
+++ lib/features/insights/domain/financial_insight_engine.dart
@@ -1425,7 +1425,7 @@ class FinancialInsightEngine {
       .trim()
       .toLowerCase()
       .replaceAll(RegExp(r'[\s_\-·/]+'), '')
-      .replaceAll(RegExp(r'银行|信用卡|银行卡|卡
+      .replaceAll(RegExp(r'银行|信用卡|银行卡|卡$'), '');
 
   FinancialInsightItem _item({
     required String id,
